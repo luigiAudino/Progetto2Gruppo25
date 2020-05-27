bef5ee500d7198e69ebc1648f520e9c7a7743259
luigiAudino <luigi.audino@icloud.com>
2020-05-27 23:17:00 +0200

    add files

---
+++ cmake-build-debug/CMakeFiles/Progetto2Gruppo25.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Progetto2Gruppo25.dir/main.c.o"
+  "Progetto2Gruppo25"
+  "Progetto2Gruppo25.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Progetto2Gruppo25.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Progetto2Gruppo25.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Progetto2Gruppo25.
+# This may be replaced when dependencies are built.
